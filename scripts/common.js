8eb1009392bfc840d9fb7358f5f1bc49dd96e5f9
artsiom <gefest_1@yahoo.com>
2016-11-24 22:46:17 +0300

    reorganize code

--- scripts/common.js
+++ scripts/common.js
@@ -5,76 +5,143 @@
 (function () {
 	"use strict";
 
-	class NewsStorageConstructor {
-		constructor() {
-			this.newsContainer = document.getElementById("articles");
-			this.key = "&apiKey=8c90eb13964a48e8ba53ef3b2a1eb61e";
-			this.url = " https://newsapi.org/v1/articles?source=";
-			this.logoUrl = "https://pp.vk.me/c624017/v624017178/e05e/zW-tDkBCYJI.jpg";
-			this.resources = {
-				abc: "abc-news-au",
-				cnn: "cnn",
-				espn: "espn",
-				focus: "focus",
-				bloomberg: "bloomberg",
-				default: "abc-news-au"
-			};
-			this.news = [];
+	class NewsStorage {
+		constructor(news, defaultNewsBlock){
+			this.news = news;
+			this.currentNewsBlock,
+			this.defaultNewsBlock = defaultNewsBlock;
 		}
-
-		createTemplate(news) {
-	    	const article = document.createElement("article");
-	    	const publishedTime = news.publishedAt ? news.publishedAt.split("T").join(" ") : "";
-	    	const html = `
-	            <header>
-	            	<div class="thumb-wrapper">
-						<img src="${news.urlToImage || this.logoUrl}" alt="">
-	            	</div>
-	                <h2><a href="${news.url}">${news.title}</a></h2>
-	                <h3>Published: ${publishedTime}</h3>
-	            </header>
-	            <div class="article-body">
-	                <p class="article-body">
-	                    ${(news.description || '')}
-	                </p>
-	            </div>`;
-	        article.innerHTML = html;
-	        this.newsContainer.appendChild(article);
-	    }
-
-	    sendRequest(resource) {
-    		return fetch(`${this.url}${resource}${this.key}`)
-		    	.then((response) => response.json())
-				.then((data) =>  this.news = data)
-		    	.catch((error) => console.log(error))
-    	}
-    	clearNews(){
-    		this.newsContainer.innerHTML = "";
-    	}
-    	render(data) {
-            data.articles.forEach((article) => this.createTemplate(article));
+		setNews(currentNewsBlock){
+			this.currentNewsBlock = currentNewsBlock;
 		}
-    	addEvents() {
-    		const navLinks = document.querySelectorAll(".nav-item");
-    		for( let item of navLinks) {
-    			const link = item.getAttribute("data-url");
-    			const resource = this.resources[link];
-    			item.addEventListener("click", (e) => {
-    				e.preventDefault();
-    				this.clearNews();
-    				this.sendRequest(resource).then(() => this.render(this.news));
-    			})
-    		}
-    	}
-    	init(){
-    		this.addEvents();
-    		this.sendRequest(this.resources.default).then(() => this.render(this.news));
-    	}
+		getNews(){
+			return this.currentNewsBlock;
+		}
 	}
 
-	const news = new NewsStorageConstructor();
+	function renderNews(news, defaultPicture) {
+		const defaultPicture = defaultPicture,
+			  news = news;
 
-	news.init();
+			  news.articles.forEach((article) => this.createTemplate(article));
+		}
+
+	}
+	function addEvents() {
+		const navLinks = document.querySelectorAll(".nav-item");
+		const newsContainer = document.getElementById("articles");
+		for( let item of navLinks) {
+			const link = item.getAttribute("data-url");
+			const resource = resources[link];
+			item.addEventListener("click", (e) => {
+				e.preventDefault();
+				clearBlock(newsContainer);
+				sendRequest(resource).then(() => renderNews(news));
+			})
+		}
+    }
+    function createTemplate(news) {
+		const article = document.createElement("article");
+		const newsContainer = document.getElementById("articles");
+		const publishedTime = news.publishedAt ? news.publishedAt.split("T").join(" ") : "";
+		const html = `
+	        <header>
+	        	<div class="thumb-wrapper">
+					<img src="${news.urlToImage || this.logoUrl}" alt="">
+	        	</div>
+	            <h2><a href="${news.url}">${news.title}</a></h2>
+	            <h3>Published: ${publishedTime}</h3>
+	        </header>
+	        <div class="article-body">
+	            <p class="article-body">
+	                ${(news.description || '')}
+	            </p>
+	        </div>`;
+	    article.innerHTML = html;
+	    newsContainer.appendChild(article);
+	}
+
+	function clearBlock(selector) {
+		selector.innerHTML = "";
+	}
+
+	function sendRequest(url, key, resources) {
+		return fetch(`${url}${resource}${key}`)
+	    	.then((response) => response.json())
+			.then((data) => data)
+	    	.catch((error) => console.log(error))
+	}
+
+	// class NewsStorageConstructor {
+	// 	constructor() {
+	// 		this.newsContainer = document.getElementById("articles");
+	// 		this.key = "&apiKey=8c90eb13964a48e8ba53ef3b2a1eb61e";
+	// 		this.url = " https://newsapi.org/v1/articles?source=";
+	// 		this.logoUrl = "https://pp.vk.me/c624017/v624017178/e05e/zW-tDkBCYJI.jpg";
+	// 		this.resources = {
+	// 			abc: "abc-news-au",
+	// 			cnn: "cnn",
+	// 			espn: "espn",
+	// 			focus: "focus",
+	// 			bloomberg: "bloomberg",
+	// 			default: "abc-news-au"
+	// 		};
+	// 		this.news = [];
+	// 	}
+
+	// 	createTemplate(news) {
+	//     	const article = document.createElement("article");
+	//     	const publishedTime = news.publishedAt ? news.publishedAt.split("T").join(" ") : "";
+	//     	const html = `
+	//             <header>
+	//             	<div class="thumb-wrapper">
+	// 					<img src="${news.urlToImage || this.logoUrl}" alt="">
+	//             	</div>
+	//                 <h2><a href="${news.url}">${news.title}</a></h2>
+	//                 <h3>Published: ${publishedTime}</h3>
+	//             </header>
+	//             <div class="article-body">
+	//                 <p class="article-body">
+	//                     ${(news.description || '')}
+	//                 </p>
+	//             </div>`;
+	//         article.innerHTML = html;
+	//         this.newsContainer.appendChild(article);
+	//     }
+
+	//    //  sendRequest(resource) {
+ //    		return fetch(`${this.url}${resource}${this.key}`)
+	// 	    	.then((response) => response.json())
+	// 			.then((data) =>  this.news = data)
+	// 	    	.catch((error) => console.log(error))
+ //    	}
+ //    	clearNews(){
+ //    		this.newsContainer.innerHTML = "";
+ //    	}
+ //    	render(data) {
+ //            data.articles.forEach((article) => this.createTemplate(article));
+	// 	}
+ //    	addEvents() {
+ //    		const navLinks = document.querySelectorAll(".nav-item");
+ //    		for( let item of navLinks) {
+ //    			const link = item.getAttribute("data-url");
+ //    			const resource = this.resources[link];
+ //    			item.addEventListener("click", (e) => {
+ //    				e.preventDefault();
+ //    				this.clearNews();
+ //    				this.sendRequest(resource).then(() => this.render(this.news));
+ //    			})
+ //    		}
+ //    	}
+ //    	init(){
+ //    		this.addEvents();
+ //    		this.sendRequest(this.resources.default).then(() => this.render(this.news));
+ //    	}
+	// }
+
+	// const news = new NewsStorageConstructor();
+
+	// news.init();
 	// const logAppConfig = {
 	// 	get (target, key) {
 	// 		if(key == "init") {
